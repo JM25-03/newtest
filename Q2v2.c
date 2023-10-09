ad89b609a193b8e9583872830da0541a8562cfc2
JM25-03 <jiaminlee2503@gmail.com>
2023-10-09 12:24:15 +0800

    commiting

---
+++ Q2v2.c
@@ -0,0 +1,32 @@
+/**************************************************/
+/*  Q2.c                                          */
+/*                                                */
+/*  This program executes the various tasks for   */
+/*  measurement, comparison and to illustrate the */
+/*  execution time of c programming               */ 
+/**************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// combined function
+int main(){
+    // Calling and executing the arithmetic functions
+
+    // Function calls
+
+    // Calling and executing the loop functions
+
+    // Calling and executing the memory access functions
+
+    return 0;
+}
+
+// Arithmetic function codes
+
+// Function codes
+
+// Loop codes
+
+// Memory codes
